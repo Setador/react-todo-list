92128c3a12e63ae42efa2fbbeed3b6c601649816
Gabriel Macias <Setador@users.noreply.github.com>
2020-01-09 01:40:51 -0800

    Updates

---
+++ precache-manifest.ac28d9ec41fe6b56cc6f67a5b52d0fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803eb2c51b17dd2365bcc14b05e2bc6e",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "d43a400d47e12bf66ed8",
+    "url": "/react-todo-list/static/css/main.2b3489b9.chunk.css"
+  },
+  {
+    "revision": "285f038744a0b751bae1",
+    "url": "/react-todo-list/static/js/2.c16ddd1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-list/static/js/2.c16ddd1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d43a400d47e12bf66ed8",
+    "url": "/react-todo-list/static/js/main.11cedfea.chunk.js"
+  },
+  {
+    "revision": "6290687cf79f8b54e61b",
+    "url": "/react-todo-list/static/js/runtime-main.2dcafc27.js"
+  }
+]);
